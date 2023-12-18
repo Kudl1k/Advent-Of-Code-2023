10de9b929e8edc5f99597c5932ae4e6e49199ae6
Kudl1k <stepanek.kudlacek@gmail.com>
2023-12-19 00:39:17 +0100

    not done yet

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -1,5 +1,74 @@
 package org.example
 
+import java.io.File
+
+
+fun loadFile(fileName: String): List<String> {
+    val lines = mutableListOf<String>()
+    val inputStream = File(fileName).inputStream()
+    inputStream.bufferedReader().forEachLine { lines.add(it) }
+    return lines
+}
+
+fun getNumber(line: String): Boolean{
+
+    var blueHighest: Int = 0
+    var redHighest: Int = 0
+    var greenHighest: Int = 0
+    var input: String = ""
+    var tempInt: String = ""
+    var game: Int = 0
+    for (i in line.indices){
+        if (line[i].isDigit()){
+            if (line[i] in '1'..'9'){
+                tempInt += line[i]
+            }
+        } else {
+            if (line[i] in 'a'..'z'){
+                input += line[i]
+            } else {
+                continue
+            }
+            when (input){
+                "red" -> {
+                    if (redHighest < tempInt.toInt()){
+                        redHighest = tempInt.toInt()
+                    }
+                    tempInt = ""
+                    input = ""
+                }
+                "green" -> {
+                    if (greenHighest < tempInt.toInt()){
+                        greenHighest = tempInt.toInt()
+                    }
+                    tempInt = ""
+                    input = ""
+                }
+                "blue" -> {
+                    if (blueHighest < tempInt.toInt()){
+                        blueHighest = tempInt.toInt()
+                    }
+                    tempInt = ""
+                    input = ""
+                }
+            }
+        }
+    }
+
+    return redHighest <= 12 && greenHighest <= 13 && blueHighest <= 14
+}
+
+
 fun main() {
-    println("Hello World!")
+    val lines = loadFile("input.txt")
+    var result = 0
+    for (i in lines.indices){
+        val game = i + 1
+        val line = lines[i].dropWhile { it != ':' }
+        if (getNumber(line)){
+            println(game)
+            result += game
+        }
+    }
+    println(result)
 }

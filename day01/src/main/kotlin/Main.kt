6990bb35f5aa281cb0921f90565c09f67e784056
Kudl1k <stepanek.kudlacek@gmail.com>
2023-12-18 23:57:34 +0100

    done

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,5 +1,47 @@
 package org.example
 
+import java.io.File
+
+fun loadFile(fileName: String): List<String> {
+    val lines = mutableListOf<String>()
+    val inputStream = File(fileName).inputStream()
+    inputStream.bufferedReader().forEachLine { lines.add(it) }
+    return lines
+}
+
+fun getNumber(line: String): Int{
+    var number = ""
+    for (i in line.indices){
+        if (!line[i].isDigit()){
+            continue
+        }
+        if (line[i] in '0'..'9'){
+            number += line[i]
+            break
+        }
+    }
+    for (i in line.length-1 downTo 0){
+        if (!line[i].isDigit()){
+            continue
+        }
+        if (line[i] in '0'..'9'){
+            number += line[i]
+            break
+        }
+    }
+    return if (number.isBlank()){
+        0
+    } else {
+        number.toInt()
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    val lines = loadFile("input.txt")
+    val listOfNums = mutableListOf<Int>()
+
+    lines.forEach {
+        listOfNums.add(getNumber(it))
+    }
+    println(listOfNums.sum())
 }

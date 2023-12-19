75304ba705d4603ed72d0e2afa1f533238f34aa1
Kudl1k <stepanek.kudlacek@gmail.com>
2023-12-19 15:30:54 +0100

    second part done, Day 02 done

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -10,7 +10,7 @@ fun loadFile(fileName: String): List<String> {
     return lines
 }
 
-fun getNumber(line: String): Boolean{
+fun getPossibleGames(line: String): Boolean{
 
 
     var input: String = ""
@@ -33,41 +33,26 @@ fun getNumber(line: String): Boolean{
             }
             when (input){
                 "red" -> {
-                    redCounter += tempInt.toInt()
                     if (12 < tempInt.toInt()){
-                        print("$tempInt red max value")
                         return false
                     }
-                    if (12 < redCounter){
-                        print("$redCounter red sum value")
-                        return false
-                    }
+
                     tempInt = ""
                     input = ""
                 }
                 "green" -> {
-                    greenCounter += tempInt.toInt()
                     if (13 < tempInt.toInt()){
-                        print("$tempInt green max value")
                         return false
                     }
-                    if (13 < greenCounter){
-                        print("$greenCounter green sum value")
-                        return false
-                    }
+
                     tempInt = ""
                     input = ""
                 }
                 "blue" -> {
-                    blueCounter += tempInt.toInt()
                     if (14 < tempInt.toInt()){
-                        print("$tempInt blue max value")
                         return false
                     }
-                    if (14 < blueCounter){
-                        print("$blueCounter blue sum value")
-                        return false
-                    }
+
                     tempInt = ""
                     input = ""
                 }
@@ -84,19 +69,80 @@ fun getNumber(line: String): Boolean{
     return true
 }
 
+fun getMinimumPossible(line: String): Triple<Int,Int,Int>{
+
+    var input: String = ""
+    var tempInt: String = ""
+
+    var redCounter: Int = 0
+    var greenCounter: Int = 0
+    var blueCounter: Int = 0
+
+    for (i in line.indices){
+        if (line[i].isDigit()){
+            tempInt += line[i]
+        } else {
+            if (line[i] in 'a'..'z' || line[i] == ';'){
+                input += line[i]
+            } else {
+                continue
+            }
+            when (input){
+                "red" -> {
+                    if (tempInt.toInt() > redCounter){
+                        redCounter = tempInt.toInt()
+                    }
+                    tempInt = ""
+                    input = ""
+                }
+                "green" -> {
+                    if (tempInt.toInt() > greenCounter){
+                        greenCounter = tempInt.toInt()
+                    }
+                    tempInt = ""
+                    input = ""
+                }
+                "blue" -> {
+                    if (tempInt.toInt() > blueCounter){
+                        blueCounter = tempInt.toInt()
+                    }
+
+                    tempInt = ""
+                    input = ""
+                }
+                ";" -> {
+                    tempInt = ""
+                    input = ""
+                }
+            }
+        }
+    }
+    return Triple(redCounter,greenCounter,blueCounter)
+}
+
+
 
 fun main() {
     val lines = loadFile("input.txt")
-    var result = 0
+    var sumOfPossibleGames = 0
     for (i in lines.indices){
         val game = i + 1
         val line = lines[i].dropWhile { it != ':' }.drop(1)
-        print("game $game :")
-
-        if (getNumber(line)){
-            result += game
+        if (getPossibleGames(line)){
+            sumOfPossibleGames += game
         }
-        println()
     }
-    println(result)
+
+    var sumOfCubes = 0
+    var temp = 0
+    for (i in lines.indices){
+        val line = lines[i].dropWhile { it != ':' }.drop(1)
+        val cubes = getMinimumPossible(line)
+        temp = cubes.first * cubes.second * cubes.third
+        sumOfCubes += temp
+        temp = 0
+    }
+
+    println(sumOfPossibleGames)
+    println(sumOfCubes)
 }

c822b8bc7a66937b67146a1a2e67e2e71c20b78f
Kudl1k <stepanek.kudlacek@gmail.com>
2023-12-21 00:24:26 +0100

    first part done

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,5 +1,53 @@
 package org.example
 
+import java.io.File
+import kotlin.time.times
+
+
+fun loadFile(fileName: String): List<String> {
+    val lines = mutableListOf<String>()
+    val inputStream = File(fileName).inputStream()
+    inputStream.bufferedReader().forEachLine { lines.add(it) }
+    return lines
+}
+
+
+fun getTimeAndDistance(lines: List<String>):List<Pair<Int,Int>>{
+    val timeAndDistance = mutableListOf<Pair<Int,Int>>()
+
+
+    val time: List<String> = lines[0].split("\\s+".toRegex()).drop(1)
+    val distance: List<String> = lines[1].split("\\s+".toRegex()).drop(1)
+
+    for (i in time.indices){
+        timeAndDistance.add(Pair(time[i].toInt(),distance[i].toInt()))
+    }
+    return timeAndDistance
+}
+
+fun getNumberOfWays(table: List<Pair<Int,Int>>): Int{
+    val listOfPossibleWays = mutableListOf<Int>()
+    var possibleWays = 0
+    for (i in table.indices){
+        for (j in 0..table[i].first){
+            if (j*(table[i].first-j) > table[i].second){
+                possibleWays++
+            }
+        }
+        listOfPossibleWays.add(possibleWays)
+        possibleWays = 0
+    }
+    var result = 1
+    for (i in listOfPossibleWays.indices){
+        result *= listOfPossibleWays[i]
+    }
+    return result
+}
+
+
+
 fun main() {
-    println("Hello World!")
+    val lines = loadFile("input.txt")
+    val table = getTimeAndDistance(lines)
+    println(getNumberOfWays(table))
 }
